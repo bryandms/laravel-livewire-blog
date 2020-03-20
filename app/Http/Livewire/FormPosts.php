86a6e8fd3ec3d7f9c9a1353a0c2fde219ffce307
Bryan Morales <bryan.moralessanchez@ucr.ac.cr>
2020-03-19 20:28:41 -0600

    Add form posts

--- app/Http/Livewire/FormPosts.php
+++ app/Http/Livewire/FormPosts.php
@@ -3,9 +3,39 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Post;
 
 class FormPosts extends Component
 {
+    public $title = '';
+    public $body  = '';
+
+    public function update($field)
+    {
+        $this->validateOnly($field, [
+            'title' => 'required|min:2|unique:posts',
+            'body'  => 'required|min:2',
+        ]);
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            'title' => 'required|min:2|unique:posts',
+            'body'  => 'required|min:2',
+        ]);
+
+        Post::create([
+            'user_id' => auth()->id(),
+            'title'   => $this->title,
+            'body'    => $this->body,
+        ]);
+
+        session()->flash('message', 'The post has been created successfully');
+        $this->emit('searchingPosts');
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.form-posts');
--- resources/views/livewire/form-posts.blade.php
+++ resources/views/livewire/form-posts.blade.php
@@ -1,3 +1,44 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <h2 class="text-muted text-center">Form post</h2>
+
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+    <form wire:submit.prevent="submit">
+        <div class="form-group">
+            <label>Title</label>
+            <input
+                autocomplete="off"
+                type="text"
+                class="form-control"
+                name="title"
+                :key="'title'"
+                wire:model.debounce.500ms="title"
+            />
+
+            @error('title')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label>Post</label>
+            <textarea
+                class="form-control"
+                name="body"
+                :key="'body'"
+                rows="3"
+                wire:model.debounce.500ms="body"
+            ></textarea>
+
+            @error('body')
+                <span class="text-danger">{{ $body }}</span>
+            @enderror
+        </div>
+
+        <button class="btn btn-block btn-primary" type="submit">Create</button>
+    </form>
 </div>
