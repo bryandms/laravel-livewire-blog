fa0c7426730ed1ad11fa6a430df54e2ce4ccd5ec
Bryan Morales <bryan.moralessanchez@ucr.ac.cr>
2020-03-19 20:28:02 -0600

    Add search posts

--- app/Http/Livewire/SearchPosts.php
+++ app/Http/Livewire/SearchPosts.php
@@ -6,6 +6,21 @@ use Livewire\Component;
 
 class SearchPosts extends Component
 {
+    public $search = '';
+
+    public function mount()
+    {
+        $this->search = session('search');
+    }
+
+    public function updatedSearch($search)
+    {
+        $this->search = $search;
+        session()->put('search', $search);
+        session()->save();
+        $this->emit('searchingPosts');
+    }
+
     public function render()
     {
         return view('livewire.search-posts');
--- resources/views/livewire/search-posts.blade.php
+++ resources/views/livewire/search-posts.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="row mt-4">
+    <div class="col-12">
+        <h2 class="text-muted text-center">Post finder</h2>
+
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    @if ($search !== "")
+                        Searching for {{ $search }}
+                    @else
+                        Search
+                    @endif
+                </div>
+
+                <div class="card-text">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-search"></i></span>
+                        </div>
+                        <input wire:model.debounce.500ms="search" class="form-control" type="text" name="search">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
